a77a79fff14e0deb85757fb63eb48d7bac3fcf30
polarius <tarasov.slavas2002@gmail.com>
2025-02-02 13:00:04 +0300

    Инициировал проект

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1 class="header__title">Hello world</h1>
+</template>
+
+<style scoped lang="scss">
+.header {
+  &__title {
+    font-size: 2rem;
+    color: pink;
+  }
+}
+</style>
