59287501ef23c1b7395b3ff972093df0894f726e
polarius <tarasov.slavas2002@gmail.com>
2025-02-02 13:43:22 +0300

    Добавил базовые настройки под pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+      vue(),
+      VitePWA({
+          includeAssets: ['vite.svg', 'vite.svg', 'vite.svg'],
+          manifest: {
+              name: 'FineW',
+              short_name: 'FineW',
+              description: 'Finance control, finance watcher',
+              theme_color: '#ffffff',
+              icons: [
+                  {
+                      src: 'vite.svg',
+                      sizes: '192x192',
+                      type: 'image/svg'
+                  },
+                  {
+                      src: 'vite.svg',
+                      sizes: '512x512',
+                      type: 'image/svg'
+                  }
+              ]
+          }
+      }),
+  ]
 })
